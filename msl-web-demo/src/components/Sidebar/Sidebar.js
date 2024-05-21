c72d185f29bf274cd854f15d40798fce49413806
kelseyvn17 <s3904681@rmit.edu.vn>
2024-05-22 04:20:57 +1000

    add logo and create sidebar and sidebardata

---
+++ msl-web-demo/src/components/Sidebar/Sidebar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./Sidebar.css"
+import { SidebarData } from './SidebarData'
+
+function Sidebar(){
+    return (
+        <div className='Sidebar'>
+            <div className='logo'>
+                <img src='img/logo.png'></img>
+            </div>
+        </div>
+    )
+}
+
+export default Sidebar
---
+++ msl-web-demo/src/components/Sidebar/SidebarData.js
@@ -0,0 +1,29 @@
+import React from "react";
+export const SidebarData = [
+    {
+        title: "Dashboard",
+        icon: "img/logo.png",
+        link: "/"
+    },
+    {
+        title: "Inbox",
+        icon: "",
+        link: "/"
+    },
+    {
+        title: "Assignment",
+        icon: "",
+        link: "/"
+    },
+    {
+        title: "Studio",
+        icon: "",
+        link: "/"
+    },
+    {
+        title: "Help",
+        icon: "",
+        link: "/"
+    }
+
+];
